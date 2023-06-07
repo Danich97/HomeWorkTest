45a8927df7d8418af701b8edf60fc16376139d3f
Danich97 <danilashariy23@gmail.com>
2023-06-07 20:19:10 +0300

    new home work

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = 78945;
+
+int firstNumber = x / 10000;
+int secondNumber = (x / 1000 - firstNumber * 10);
+int thirdNumber = (x - firstNumber * 10000 - secondNumber * 1000) / 100;
+int fourthNumber = (x - firstNumber * 10000 - secondNumber * 1000 - thirdNumber * 100) / 10;
+int fifthNumber = (x - firstNumber * 10000 - secondNumber * 1000 - thirdNumber * 100 - fourthNumber * 10);
+
+Console.WriteLine(firstNumber);
+Console.WriteLine(secondNumber);
+Console.WriteLine(thirdNumber);
+Console.WriteLine(fourthNumber);
+Console.WriteLine(fifthNumber);
--- testtestes/testtestes/Program.cs
+++ testtestes/testtestes/Program.cs
@@ -1,10 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
 
-jksdgkjhsdghj
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть слово:");
+        string word = Console.ReadLine(); // Зчитування введеного користувачем слова
 
-    lkjsdfgjklh
+        string image = ""; // Змінна для збереження псевдографічного зображення
 
-    sdkgjjshg
+        // Формування псевдографічного зображення на основі введеного слова
+        foreach (char c in word)
+        {
+            switch (c)
+            {
+                case 'A':
+                    image += "  A  \n A A \nAAAAA\nA   A\nA   A\n\n";
+                    break;
+                case 'B':
+                    image += "BBBB \nB   B\nBBBB \nB   B\nBBBB \n\n";
+                    break;
+                // Додайте інші символи за необхідністю
+                default:
+                    image += "Невідомий символ\n\n";
+                    break;
+            }
+        }
 
-    sdlgkjlsdgj
+        Console.WriteLine(image);
+    }
+}
